d0b5a4d457fbe937381cda5b7a3219af988dc42b
Matěj Štágl <stagl.matej@centrum.cz>
2019-02-14 00:58:13 +0100

    Added DarkMouseTool

--- DarkUI/Controls/DarkMouseTool.cs
+++ DarkUI/Controls/DarkMouseTool.cs
@@ -9,7 +9,7 @@ namespace DarkUI.Controls
 {
     [ToolboxBitmap(typeof(Button))]
     [DefaultEvent("Click")]
-    public class DarkColorPallete : Button
+    public class DarkMouseTool : Button
     {
         #region Field Region
 
@@ -23,9 +23,6 @@ namespace DarkUI.Controls
         private const int _padding = Consts.Padding / 2;
         private DarkComboBox darkComboBox1;
         private int _imagePadding = 5; // Consts.Padding / 2
-
-        public Color[] colors = new Color[50];
-      
 
         #endregion
 
@@ -106,6 +103,9 @@ namespace DarkUI.Controls
 
         #region Code Property Region
 
+        public Color LeftColor { get; set; }
+        public Color RightColor { get; set; }
+
         [Browsable(false)]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
         public new bool AutoEllipsis
@@ -166,7 +166,7 @@ namespace DarkUI.Controls
 
         #region Constructor Region
 
-        public DarkColorPallete()
+        public DarkMouseTool()
         {
             SetStyle(ControlStyles.OptimizedDoubleBuffer |
                      ControlStyles.ResizeRedraw |
@@ -178,69 +178,7 @@ namespace DarkUI.Controls
             SetButtonState(DarkControlState.Normal);
             Padding = new Padding(_padding);
 
-            // row 1
-            colors[0] = Color.Red;
-            colors[1] = Color.Yellow;
-            colors[2] = Color.Lime;
-            colors[3] = Color.Aqua;
-            colors[4] = Color.Blue;
-            colors[5] = Color.Fuchsia;
-
-            // row 2
-            colors[6] = Color.FromArgb(64, 0 ,0);
-            colors[7] = Color.FromArgb(64, 64, 0);
-            colors[8] = Color.FromArgb(0, 64, 0);
-            colors[9] = Color.FromArgb(0, 64, 64);
-            colors[10] = Color.FromArgb(0, 0, 64);
-            colors[11] = Color.FromArgb(64, 0, 64);
-
-            // row 3
-            colors[12] = Color.FromArgb(128, 0, 0);
-            colors[13] = Color.FromArgb(128, 128, 0);
-            colors[14] = Color.FromArgb(0, 128, 0);
-            colors[15] = Color.FromArgb(0, 128, 128);
-            colors[16] = Color.FromArgb(0, 0, 128);
-            colors[17] = Color.FromArgb(128, 0, 128);
-
-            // row 4
-            colors[18] = Color.FromArgb(192, 0, 0);
-            colors[19] = Color.FromArgb(192, 192, 0);
-            colors[20] = Color.FromArgb(0, 192, 0);
-            colors[21] = Color.FromArgb(0, 192, 192);
-            colors[22] = Color.FromArgb(0, 0, 192);
-            colors[23] = Color.FromArgb(192, 0, 192);
-
-            // row 5
-            colors[24] = Color.FromArgb(255, 64, 64);
-            colors[25] = Color.FromArgb(255, 255, 64);
-            colors[26] = Color.FromArgb(64, 255, 64);
-            colors[27] = Color.FromArgb(64, 255, 255);
-            colors[28] = Color.FromArgb(64, 64, 255);
-            colors[29] = Color.FromArgb(255, 64, 255);
-
-            // row 6
-            colors[30] = Color.FromArgb(255, 128, 128);
-            colors[31] = Color.FromArgb(255, 255, 128);
-            colors[32] = Color.FromArgb(128, 255, 128);
-            colors[33] = Color.FromArgb(128, 255, 255);
-            colors[34] = Color.FromArgb(128, 128, 255);
-            colors[35] = Color.FromArgb(255, 128, 255);
-
-            // row 7
-            colors[36] = Color.FromArgb(255, 192, 192);
-            colors[37] = Color.FromArgb(255, 255, 192);
-            colors[38] = Color.FromArgb(192, 255, 192);
-            colors[39] = Color.FromArgb(192, 255, 255);
-            colors[40] = Color.FromArgb(192, 192, 255);
-            colors[41] = Color.FromArgb(255, 192, 255);
-
-            // row 8
-            colors[42] = Color.FromArgb(0, 0, 0);
-            colors[43] = Color.FromArgb(32, 32, 32);
-            colors[44] = Color.FromArgb(64, 64, 64);
-            colors[45] = Color.FromArgb(128, 128, 128);
-            colors[46] = Color.FromArgb(192, 192, 192);
-            colors[47] = Color.FromArgb(255, 255, 255);
+          
         }
 
         #endregion
@@ -409,6 +347,9 @@ namespace DarkUI.Controls
             var fillColor = _useGenericBackColor ? (_isDefault ? Colors.DarkBlueBackground : Colors.LightBackground) : BackColor;
             var hoverColor = _useGenericBackColor ? (_isDefault ? Colors.BlueBackground : Colors.LighterBackground) : ControlPaint.Light(BackColor);
 
+            Brush cb = new SolidBrush(Color.FromArgb(60, 63, 65));
+            g.FillRectangle(cb, rect);
+
             if (Enabled)
             {
                 switch (ButtonStyle)
@@ -449,161 +390,24 @@ namespace DarkUI.Controls
                 fillColor = Colors.DarkGreySelection;
             }
 
-            using (var b = new SolidBrush(Color.Red))
-            {
                 Pen pp = new Pen(Color.Black);
 
                 int xx = rect.X;
-                int yy = rect.Y;
+                int yy = rect.Y + 16;
                 int xo = xx;
-                int size = 16;
+                int size = 48;
                 int index = 0;
 
-                if (!Gradient)
-                {
-                    for (int i = 0; i < 8; i++)
-                    {
-                        for (int j = 0; j < 6; j++)
-                        {
-                            rect.X = xx;
-                            rect.Y = yy;
-                            rect.Width = size;
-                            rect.Height = size;
+                Brush bb = new SolidBrush(LeftColor);
+                g.FillRectangle(bb, new Rectangle(xx, yy, 48, 48));
+                g.DrawRectangle(pp, new Rectangle(xx, yy, 48, 48));
+                bb = new SolidBrush(RightColor);
+                g.FillRectangle(bb, new Rectangle(xx + 50, yy, 48, 48));
+                g.DrawRectangle(pp, new Rectangle(xx + 50, yy, 48, 48));
 
-                            b.Color = colors[index];
-                            g.FillRectangle(b, rect);
-                            g.DrawRectangle(pp, rect);
-
-                            index++;
-                            xx += size;
-                        }
-
-                        yy += size;
-                        xx = xo;
-                    }
-                }
-                else
-                {
-                    ColorBlend cb = new ColorBlend();
-                    cb.Positions = new[] {0, 0.2F, 0.4F, 0.6F, 0.8F, 0.9F, 1};
-                    cb.Colors = new[] {Color.Red, Color.Yellow, Color.Lime, Color.Cyan, Color.Blue, Color.Fuchsia, Color.Red};
-
-                    LinearGradientBrush linGrBrush = new LinearGradientBrush(
-                        new Point(65, 0),
-                        new Point(65, 159),
-                        Color.FromArgb(255, 255, 0, 0),   
-                        Color.FromArgb(255, 0, 0, 255));  
-
-                    linGrBrush.InterpolationColors = cb;
-
-                    rect.X = xx;
-                    rect.Y = yy;
-                    rect.Width = 129;
-                    rect.Height = 159;
-                    g.FillRectangle(linGrBrush, rect);
-
-                    cb.Positions = new[] { 0, 0.5F, 1F };
-                    cb.Colors = new[] { Color.Black, Color.FromArgb(128, 0, 0, 0), Color.Transparent };
-
-                    linGrBrush = new LinearGradientBrush(new Rectangle(0, 0, 70, 160), 
-                        Color.FromArgb(255, 255, 0, 0), 
-                        Color.FromArgb(0, 0, 0, 255), LinearGradientMode.Horizontal);  
-
-                    linGrBrush.InterpolationColors = cb;
-                    linGrBrush.GammaCorrection = true;
-
-
-                    rect.Width = 70;
-                    g.FillRectangle(linGrBrush, rect);
-
-                    cb.Positions = new[] { 0, 0.5F, 1F };
-                    cb.Colors = new[] { Color.Transparent, Color.FromArgb(128, 255, 255, 255), Color.White };
-                    linGrBrush.InterpolationColors = cb;
-
-                    rect.X = xx + 70;
-                    g.FillRectangle(linGrBrush, rect);
-
-                    rect.X = xx;
-                    rect.Y = yy;
-                    rect.Width = 129;
-                    rect.Height = 159;
-                    g.DrawRectangle(pp, rect);
-                }
-
-            }
-           /*
-            if (ButtonStyle == DarkButtonStyle.Normal)
-            {
-                using (var p = new Pen(borderColor, 1))
-                {
-                    var modRect = new Rectangle(rect.Left, rect.Top, rect.Width - 1, rect.Height - 1);
-
-                    g.DrawRectangle(p, modRect);
-                }
-            }
-
-            var textOffsetX = 0;
-            var textOffsetY = 0;
-
-            if (Image != null)
-            {
-                var stringSize = g.MeasureString(Text, Font, rect.Size);
-
-                var x = ClientSize.Width / 2 - Image.Size.Width / 2;
-                var y = ClientSize.Height / 2 - Image.Size.Height / 2;
-
-                switch (TextImageRelation)
-                {
-                    case TextImageRelation.ImageAboveText:
-                        textOffsetY = Image.Size.Height / 2 + ImagePadding / 2;
-                        y = y - ((int)(stringSize.Height / 2) + ImagePadding / 2);
-                        break;
-                    case TextImageRelation.TextAboveImage:
-                        textOffsetY = (Image.Size.Height / 2 + ImagePadding / 2) * -1;
-                        y = y + (int)(stringSize.Height / 2) + ImagePadding / 2;
-                        break;
-                    case TextImageRelation.ImageBeforeText:
-                        textOffsetX = Image.Size.Width + ImagePadding / 2;
-                        x = ImagePadding;
-                        break;
-                    case TextImageRelation.TextBeforeImage:
-                        x = x + (int)stringSize.Width;
-                        break;
-                }
-
-                //g.DrawImage(Image, x, y);
-                g.DrawImage(Image, new Rectangle(x, y, Image.Width, Image.Height));
-            }
-
-            using (var b = new SolidBrush(textColor))
-            {
-                var modRect = new Rectangle(rect.Left + textOffsetX + Padding.Left,
-                                            rect.Top + textOffsetY + Padding.Top, rect.Width - Padding.Horizontal,
-                                            rect.Height - Padding.Vertical);
-
-                if (Image != null)
-                {
-                    var stringFormat = new StringFormat
-                    {
-                        LineAlignment = StringAlignment.Center,
-                        Alignment = StringAlignment.Near,
-                        Trimming = StringTrimming.EllipsisCharacter
-                    };
-
-                    g.DrawString(Text, Font, b, modRect, stringFormat);
-                }
-                else
-                {
-                    var stringFormat = new StringFormat
-                    {
-                        LineAlignment = StringAlignment.Center,
-                        Alignment = StringAlignment.Center,
-                        Trimming = StringTrimming.EllipsisCharacter
-                    };
-
-                    g.DrawString(Text, Font, b, modRect, stringFormat);
-                }
-            }*/
+                Brush cbb = new SolidBrush(Colors.LightText);
+                g.DrawString("Left:", DefaultFont, cbb, new PointF(xx, yy - 16));
+                g.DrawString("Right:", DefaultFont, cbb, new PointF(xx + 50, yy - 16));
         }
 
         #endregion
